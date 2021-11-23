887a0dc2398cfcc26d5430e671c379914b6b6fa5
genos15 <tondjilee@gmail.com>
2021-11-23 11:37:51 +0300

    Designer part to do - get data from dribbble

--- src/sections/experiences-section/index.tsx
+++ src/sections/experiences-section/index.tsx
@@ -2,19 +2,26 @@ import React, {memo} from 'react';
 import styled from "styled-components";
 import {Element} from "react-scroll";
 import 'react-vertical-timeline-component/style.min.css';
-import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
+import {VerticalTimeline} from "react-vertical-timeline-component";
 import ExperienceItem from "./children/experience-item";
-import {FaUser} from "react-icons/all";
 
 const ExperiencesUIWrapper = styled.section`
   color: ${(props) => (props.theme.textColor)};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-  h2 {
+  h3, h2 {
     text-align: center;
     font-size: 2em;
     margin-bottom: .2em;
     z-index: 99;
     color: ${(props) => (props.theme.fourth)};
+  }
+  
+  h2 {
+    border: 3px solid #fff;
+    padding: .5em 1em;
   }
 
   p {
@@ -36,8 +43,11 @@ const ExperiencesUI = ({name}: ExperiencesUIProps) => {
     return <>
         <Element name={name}>
             <ExperiencesUIWrapper>
-                <h2>Where I worked</h2>
+                <h3>Where I worked</h3>
                 <p>I have worked for a lot of company in my career, and here is some of them.</p>
+
+                <h2>As Developer</h2>
+
                 <VerticalTimeline className={"timeline-wrapper"}>
                     <ExperienceItem period={"2011 - present"} position="Creative Director" companyName={"Miami, FL"}
                                     myTasks={"Creative Direction, User Experience, Visual Design, Project Management, Team Leading"}/>
@@ -47,9 +57,12 @@ const ExperiencesUI = ({name}: ExperiencesUIProps) => {
                                     myTasks={"Creative Direction, User Experience, Visual Design, Project Management, Team Leading"}/>
                     <ExperienceItem period={"2011 - present"} position="Creative Director" companyName={"Miami, FL"}
                                     myTasks={"Creative Direction, User Experience, Visual Design, Project Management, Team Leading"}/>
-                    <ExperienceItem period={"2011 - present"} position="Creative Director" companyName={"Miami, FL"}
-                                    myTasks={"Creative Direction, User Experience, Visual Design, Project Management, Team Leading"}/>
                 </VerticalTimeline>
+
+                <h2>As Designer</h2>
+                <p>I'm really passionated by design and everything related to it and here is some of my latest works</p>
+
+                {/*Dribbble api*/}
             </ExperiencesUIWrapper>
         </Element>
     </>
