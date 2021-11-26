7a3c8777ebd6515cb57f7e4b48b58f057a0919fa
Lee Niat <tondjilee@gmail.com>
2021-11-27 00:03:04 +0300

    deployment config added

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+rm -rf ./build
+yarn build
---
+++ install.dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt update
+sudo apt install nodejs
+
+sudo apt update
+sudo apt install npm
